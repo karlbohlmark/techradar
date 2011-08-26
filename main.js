7f3f2cfd6c625f5d9ff9ca39d0541cd77adcc22e
Karl Böhlmark <karl.bohlmark@gmail.com>
2011-08-26 22:26:56 +0200

    persistence

--- main.js
+++ main.js
@@ -1,41 +1,17 @@
 window.$ = require('./node_modules/jquery/dist/node-jquery');
-window._ = require('./node_modules/underscore');
-window.Backbone = require('./node_modules/backbone');
-require('./lib/storage/Store');
-
-require('./lib/model/Tech');
+var t = require('transformjs');
+require('./lib/storage/sync');
+var bus = require('./lib/bus').bus;
+var radar = require('./lib/model/radar').radar;
+var radarview =require('./lib/view/radarview.js');
 var techview =require('./lib/view/techview.js');
-require('./lib/collection/TechList.js');
-
-var techs = new TechList();
-techs.add(new Tech());
-
-
-var data = [
-    {x:540, y:440, symbol:'circle', title: 'git'},
-    {x:380, y:390, symbol: 'triangle', title: 'dojo'},
-    {x:400, y:300, symbol: 'triangle', title: 'websockets'}];
-
-function render(){
-    techview.drawDots(data);
-    var str="";
-    data.forEach(function(i){
-        str +='<li>' + i.title  + '</li>'
-    })
-    $('.techs').html(str);
-}
-
-
-
-
-render();
 
 $('input').keydown(function(e){
     if(e.keyCode==13){
         var val = $(this).val();
         $(this).val('');
-        data.push({x:200, y:200, symbol: 'circle', title: val});
-        render();
+        
+        bus.emit('new-tech', {x:600, y:200, symbol: 'circle', title: val})
     }
 })
 
--- server.js
+++ server.js
@@ -1,11 +1,79 @@
 var express = require('express');
-var server = express.createServer();
+var app = express.createServer(),
+    io = require('socket.io').listen(app);
+var http = require('http');
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
+var dbname = 'radar.db';
 
-server.use(express.static(__dirname));
-server.use(require('browserify')({
+var db;
+var exists = path.existsSync(dbname);
+if(exists){
+  db = fs.readFileSync(dbname);
+  console.log('file exists')
+  if(db.length>0)
+    db = JSON.parse(db);
+  else
+    db = { radar:[]}
+}else{
+  db = {radar:[]}
+}
+
+setInterval(function(){
+  console.log('writing db file')
+  fs.writeFileSync(dbname, JSON.stringify(db));
+  console.log('wrote db file')
+},5000)
+
+http.createServer(function(req, res){
+  console.log(req.url);
+  if (req.method === 'GET' && /^\/couch/.test(req.url)) {
+    var r = request.get('http://localhost:5984' + req.url.replace('/couch', '')) 
+    req.pipe(r);
+    r.pipe(res);
+  }else{
+    r = request.get('http://localhost:9393' + req.url);
+    req.pipe(r);
+    r.pipe(res);
+  }  
+}).listen(8001);
+
+
+app.use(express.static(__dirname));
+app.use(express.bodyParser());
+app.use(require('browserify')({
     entry : __dirname + '/main.js',
     watch : true
 }));
 
-server.listen(9393);
-console.log('Listening on 9393...');
+io.sockets.on('connection', function (socket) {
+  socket.emit('radar', db.radar);
+  socket.on('radar', function (data) {
+    console.log(data);
+  });
+
+  socket.on('tech-added',function(t){
+    console.log('tech added');
+    db.radar.push(t)
+    t.remote = true;
+    socket.broadcast.emit('new-tech', t);
+  });
+
+
+  socket.on('tech-moved',function(t){
+    console.log('tech moved');
+    db.radar.forEach(function(tech){
+      if(tech.id == t.id){
+        tech.x = t.x;
+        tech.y = t.y;
+      }
+    })
+    t.remote = true;
+    socket.broadcast.emit('tech-moved', t);
+  });
+});
+
+
+app.listen(9393);
+console.log('Listening on 8001...');
